2752aed486bec12995b8dcd8d5419c592720101f
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-01 12:24:02 +0530

    Add files via upload

---
+++ range_data_type.py
@@ -0,0 +1,44 @@
+# for  abc in range(7):
+#     print(abc)
+
+# for a in range(2,9):
+#     print(a,end=(" 😊"))
+
+# a=range(10)
+# for m in a:
+#     print(m,end=('  '))
+
+
+# for a in range(1,100):
+#     print(a,end=("  "))
+
+
+# a=True
+# print(a)
+
+
+# a={1,2,3,4,5,6}
+# print(a)
+# print(*a)
+
+
+
+
+# a=65
+# print(ord('a'))
+
+
+# print(chr(104))
+# print(ord("h"))
+
+
+# for a in range (8,8):
+#   print(a)
+
+
+
+# for a in range (10,0,-2):
+#    print(a,end=(' '))
+
+for a in range (25,3,-4):
+    print(a)
